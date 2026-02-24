0d50764c527e6e8e0792f77b0e4793d9a8a86875
Vamsikrishna Venkat <vamsikrishna.venkat@aspiresys.com>
2026-02-24 11:34:40 +0530

    running in aspire

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.get("/health", (req, res) => {
   res.status(200).json({
     status: "UP",
-    message: "Server is healthy and running in aspire",
+    message: "Server is healthy and running in aspires",
     timestamp: new Date().toISOString(),
   });
 });
